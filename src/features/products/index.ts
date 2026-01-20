b4f3804d67ea6320948281de7397b905c645c182
dotan <dotcompanyofficial@gmail.com>
2026-01-20 22:17:37 +0200

    deleting unused imports

---
+++ src/features/products/index.ts
@@ -0,0 +1,5 @@
+export { ProductCard } from './components/ProductCard/ProductCard';
+export { ProductGrid} from './components/ProductGrid/ProductGrid';
+export { useProductStore } from './productStore';
+export { productApi } from './api/productApi';
+export type { Product, ProductsResponse, ProductFilters as ProductFilterParams, ProductState } from './types/product.types';
