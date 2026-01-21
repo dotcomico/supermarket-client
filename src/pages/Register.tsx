3e5f0467deb592e3ecede1f2189be4809ab60b9d
dotan <dotcompanyofficial@gmail.com>
2026-01-21 18:41:52 +0200

    create register page to perform register form

---
+++ src/pages/Register.tsx
@@ -0,0 +1,11 @@
+import { RegisterForm } from '../features/auth/components/RegisterForm'
+
+const Register = () => {
+  return (
+    <div>
+        <RegisterForm/>
+    </div>
+  )
+}
+
+export default Register
