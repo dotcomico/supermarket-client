fddbb56479a4ec5e00a992d57ad15b92baa79174
dotan <dotcompanyofficial@gmail.com>
2026-01-22 19:54:05 +0200

    add index to includ all cart logic

---
+++ src/features/cart/index.ts
@@ -0,0 +1,5 @@
+export { CartItemCard } from './components/CartItemCard/CartItemCard';
+export { CartSummary } from './components/CartSummary/CartSummary';
+export { EmptyCart } from './components/EmptyCart/EmptyCart';
+export { useCart } from './hooks/useCart';
+export type { CartItem, CartState } from './types/cart.type';
